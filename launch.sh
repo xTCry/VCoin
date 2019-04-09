6557ee9a7ff2b42641d5f77d2d4d69e520259e47
Akionka <42269025+Akionka@users.noreply.github.com>
2019-04-10 02:17:10 +0300

    Теперь и SH умеет создавать конфиги из экзамплов

--- launch.sh
+++ launch.sh
@@ -2,5 +2,11 @@
 if [ ! -d "./node_modules/auto-updater" ]; then
 	npm i --loglevel=error
 fi
+if [ ! -f "./userconfig.json" ]; then
+	mv ./userconfig.example.json ./userconfig.json
+fi
+if [ ! -f "./botconfig.json" ]; then
+	mv ./botconfig.example.json ./botconfig.json
+fi
 node index.js
 exit 0
