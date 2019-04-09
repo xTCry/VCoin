906665e3ccb7dd5c71d1eec43b1ae703f3ba6daf
debug <cursedsealwinter@gmail.com>
2019-04-09 18:40:44 +0300

    > 1.3.9
    • Все теперь 1000% фикс, не беите.

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 @echo off
-IF NOT EXIST ./node_modules/auto-updater (npm i --loglevel=error)
+IF NOT EXIST ./node_modules/auto-updater (npm i --loglevel=error) ELSE (echo Node.JS modules are installed)
 IF NOT EXIST ./userconfig.json (RENAME "./userconfig.example.json" "userconfig.json")
 IF NOT EXIST ./botconfig.json (RENAME "./botconfig.example.json" "botconfig.json")
 title VKCoinX - Batch Script
