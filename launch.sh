8c4d356fc62f10d9c2b9bdb15274d5a1b8ac72f6
Tema Smirnov <github.tema@smirnov.one>
2019-04-10 03:54:54 +0300

    Переключение mv на cp, т.к. безопаснее
    А также сохраняется обратная совместимость, чтобы можно было при следующей обнове VCoinX сравнить конфиги между релизами

--- launch.sh
+++ launch.sh
@@ -3,10 +3,10 @@ if [ ! -d "./node_modules/auto-updater" ]; then
 	npm i --loglevel=error
 fi
 if [ ! -f "./userconfig.json" ]; then
-	mv ./userconfig.example.json ./userconfig.json
+	cp ./userconfig.example.json ./userconfig.json
 fi
 if [ ! -f "./botconfig.json" ]; then
-	mv ./botconfig.example.json ./botconfig.json
+	cp ./botconfig.example.json ./botconfig.json
 fi
 node index.js
 exit 0
